46acbb7d16064ff2466a3cfbee950b7c4ae1d0ca
sadiarehman0 <sadia.rehman1986@gmail.com>
2024-04-30 13:14:04 -0700

    push my todos list to github

---
+++ main.ts
@@ -0,0 +1,26 @@
+import inquirer from "inquirer"
+let todos = [] ;
+let condition = "true";
+while (condition)
+{
+let addTask  = await inquirer.prompt ([
+    
+        {
+             name :'todos',
+            type :'input',
+            message :"what do you want to add in your todos?"
+        },
+        
+        {
+        
+        name: 'addmore',
+        type : 'confirm',
+        message :"Do you want to add more?",
+        default :"false",
+
+       }
+    ]);
+     todos.push (addTask.todos);
+     condition = addTask.addmore
+     console.log(todos)
+}
